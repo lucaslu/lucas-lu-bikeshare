5b50ba991c6c87cdd5489238911b97c7b4b65361
Lucas Lu <lucaslu@hotmail.com>
2022-12-11 12:33:30 -0800

    change utils getRandomBikeImage function to get array of images randomly

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -1,4 +1,14 @@
 import bike1Img from "../assets/images/bike1.jpg";
+import bike2Img from "../assets/images/bike2.jpg";
+import bike3Img from "../assets/images/bike3.jpg";
+import bike4Img from "../assets/images/bike4.jpg";
+import bike5Img from "../assets/images/bike5.jpg";
+import bike6Img from "../assets/images/bike6.jpg";
+import bike7Img from "../assets/images/bike7.jpg";
+import bike8Img from "../assets/images/bike8.jpg";
+import bike9Img from "../assets/images/bike9.jpg";
+import bike10Img from "../assets/images/bike10.jpg";
+
 export const getBikes = (search, bikes) => {
   return bikes.filter((bike) => {
     return (
@@ -9,9 +19,18 @@ export const getBikes = (search, bikes) => {
 };
 
 export const getRandomBikeImage = () => {
-  // const BACKEND = "https://lucas-lu-bikeshare-api.herokuapp.com/images/";
-  // const randomImage = "bike" + (Math.floor(Math.random() * 10) + 1) + ".jpg";
-  // return BACKEND + randomImage;
-  console.log(bike1Img);
-  return bike1Img;
+  let bikeArray = [
+    bike1Img,
+    bike2Img,
+    bike3Img,
+    bike4Img,
+    bike5Img,
+    bike6Img,
+    bike7Img,
+    bike8Img,
+    bike9Img,
+    bike10Img,
+  ];
+
+  return bikeArray[Math.floor(Math.random() * 10)];
 };
